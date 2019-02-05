1311daaba5a3c0643f2de77464e371bb6bc80a1c
kouya17 <17383696+kouya17@users.noreply.github.com>
2019-02-05 21:37:41 +0900

    Add: function to get distance to ball

--- html/search/functions_1.js
+++ html/search/functions_1.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['getirseekerangle',['getIrSeekerAngle',['../d6/dda/class_ir_seeker_controller.html#a71fb9a9096eebd286ef0f02c6f603468',1,'IrSeekerController']]],
+  ['getirseekerangleanddistancebyfloat',['getIrSeekerAngleAndDistanceByFloat',['../d6/dda/class_ir_seeker_controller.html#a554c0174b99e64201156d05434781bb3',1,'IrSeekerController']]],
   ['getirseekeranglebyint',['getIrSeekerAngleByInt',['../d6/dda/class_ir_seeker_controller.html#ae8eeef8c000dbea552f843f8b97630e7',1,'IrSeekerController']]],
   ['getmotorpowers',['getMotorPowers',['../db/d49/class_u_s_b_controller.html#abd4b47f1e86159dfd7ab01ad28f21b40',1,'USBController']]],
   ['getsonardistance',['getSonarDistance',['../d0/d0d/class_ultrasonic_sensor_controller.html#ac1e9488c4e078022f294a83cc1c7fd70',1,'UltrasonicSensorController']]],
--- src/IrSeekerController.java
+++ src/IrSeekerController.java
@@ -10,6 +10,9 @@ import lejos.hardware.sensor.HiTechnicIRSeekerV2;
 import lejos.robotics.SampleProvider;
 
 public class IrSeekerController {
+    private static final int DIS_CORRECTION_COEFFICIENT = 10;
+    private static final float DIS_REFERENCE_VALUE = 1.f;
+    
     // 赤外線センサー
     private HiTechnicIRSeekerV2 irSeeker;
     private SampleProvider modulatedMode;  // 変調モード
@@ -46,5 +49,44 @@ public class IrSeekerController {
 	    return 180;
 	}
 	return (int)sampleAngle[0];
+    }
+    
+    /*!
+     * 赤外線センサーによりボールとの角度と距離をfloat型で取得する
+     * @return ボールとの角度
+     */
+    public final float[] getIrSeekerAngleAndDistanceByFloat() {
+	modulatedMode.fetchSample(sampleAngle, 0);
+	return getAngleAndDistanceFromSample(sampleAngle);
+    }
+    
+    private final float[] getAngleAndDistanceFromSample(float[] sample) {
+	float[] returnValues = new float[2];
+	float firstValue = -100.f;
+	float secondValue = -100.f;
+	int firstIndex = -1;
+	int secondIndex = -1;
+	for(int i = 0; i < sample.length; i++) {
+	    if(sample[i] > firstValue) {
+		// 元々の最大値は2番目の値として再記憶
+		secondValue = firstValue;
+		secondIndex = firstIndex;
+		// 最大値を記憶
+		firstValue = sample[i];
+		firstIndex = i;
+	    }else if(sample[i] > secondValue) {
+		secondValue = sample[i];
+		secondIndex = i;
+	    }
+	}
+	returnValues[0] = firstIndex;
+	returnValues[1] = DIS_REFERENCE_VALUE - firstValue;
+	if(Math.abs(firstIndex - secondIndex) == 1 && (firstValue - secondValue) < (firstValue / DIS_CORRECTION_COEFFICIENT)) {
+	    returnValues[0] = (firstIndex + secondIndex) / 2.f;
+	}
+	for(int i = 0; i < returnValues.length; i++) {
+	    System.out.printf("returnValues(ir)[%f] = %f¥n", i, returnValues[i]);
+	}
+	return returnValues;
     }
 }
